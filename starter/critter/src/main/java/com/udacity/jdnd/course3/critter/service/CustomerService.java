70167ea1a542ba6a43219c3ac9aad3bdf12fee78
Anoshko Alexey <AnoshkoAlexey@gmail.com>
2022-11-20 13:12:23 -0800

    Add @transactional annotation

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/service/CustomerService.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/service/CustomerService.java
@@ -3,10 +3,12 @@ package com.udacity.jdnd.course3.critter.service;
 import com.udacity.jdnd.course3.critter.entity.Customer;
 import com.udacity.jdnd.course3.critter.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class CustomerService {
     private final CustomerRepository customerRepository;
 
